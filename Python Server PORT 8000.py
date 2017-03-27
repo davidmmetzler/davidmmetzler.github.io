9cc952fe404b853499f0de6a1f8184dbba548a3b
davemetzler <sinesoma@gmail.com>
2017-03-27 11:59:36 -0600

    new site
    you know it

---
+++ Python Server PORT 8000.py
@@ -0,0 +1,13 @@
+import os
+#os.chdir('C:\Users\Dave Metzler.SUMMIT\Documents\WEBSITE')
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print ("serving at port", PORT)
+httpd.serve_forever()
